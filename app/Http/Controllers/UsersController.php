74d8c285a9f5117642412de7b61fa69d8f16a3e0
abukbt13 <abukbt13@gmail.com>
2023-06-12 08:40:16 +0300

    created superadmindashboard

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -112,4 +112,54 @@ class UsersController extends Controller
 
     }
 
+    public function show_users()
+    {
+        $role= Auth::user();
+        $role= $role->role;
+//        dd(role);
+        if($role == 'super_admin'){
+            $users=User::all();
+//            ('role','!=','super_admin')->get();
+            return response()->json($users);
+        }
+        else{
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Not authorised'
+            ]);
+        }
+
+    }
+    public function update_user(Request $request,$id)
+    {
+
+        $data = request()->all();
+        $rules = [
+            'role' => 'required',
+        ];
+        $valid = Validator::make($data, $rules);
+        if (count($valid->errors())) {
+            return response([
+                'status' => 'failed',
+                'errors' => $valid->errors()
+            ]);
+        }
+        $role= Auth::user();
+        $role= $role->role;
+//        dd(role);
+        if($role == 'super_admin'){
+            $user=User::find($id);
+            $user->role=$request->role;
+            $user->update();
+            return response()->json($user);
+        }
+        else{
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'Not authorised'
+            ]);
+        }
+
+    }
+
 }

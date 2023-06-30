d0dcf96ad7905ab690ad9cd1cfdf0177003f8e5a
Freemano The Only <abukbt13>
2023-06-30 19:41:22 +0300

    tasks created migration for project_id

--- database/migrations/2023_06_30_162916_add_project_id_to_tasks_table.php
+++ database/migrations/2023_06_30_162916_add_project_id_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddProjectIdToTable extends Migration
+class AddProjectIdToTasksTable extends Migration
 {
     /**
      * Run the migrations.

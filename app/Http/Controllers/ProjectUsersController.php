4d82243482c2f915cdb3cd4b2c6f55ca442da4e2
Freemano The Only <abukbt13>
2023-07-04 12:24:23 +0300

    user projects

--- app/Http/Controllers/ProjectUsersController.php
+++ app/Http/Controllers/ProjectUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProjectUser;
 use App\Models\User;
 use App\Models\Project;
 use Illuminate\Http\Request;
@@ -11,7 +12,7 @@ use Illuminate\Support\Facades\Validator;
 class ProjectUsersController extends Controller
 {
     public function addUserToProject()
-    {   
+    {
         $rules=[
             'project_id'=>'required',
             'user_id'=>'required'
@@ -26,20 +27,20 @@ class ProjectUsersController extends Controller
         }
             $projectId = $data['project_id'];
             $userId = $data['user_id'];
-         
-            
+
+
             $project = Project::find($projectId);
             $user = User::find($userId);
-            
+
             if (!$project || !$user) {
                 return response([
                     'status' => 'failed',
                     'message' => 'Project or User not found.',
                 ]);
             }
-            
+
             $project->users()->attach($user);
-            
+
             return response()->json([
                 'status' => 'success',
                 'message' => 'User added to the project successfully.',
@@ -59,13 +60,17 @@ class ProjectUsersController extends Controller
     public function fetchUserProjects()
 {
     $user = Auth::user();
-
-    $projects =
-     User::join('project_user', 'users.id', '=', 'project_user.user_id')
-        ->join('projects', 'project_user.project_id', '=', 'projects.id')
-        ->where('users.id', $user->id)
-        ->select('projects.name', 'projects.id', 'projects.description')
+    $user_id=$user->id;
+    $projects = ProjectUser::join('projects', 'project_user.project_id', '=', 'projects.id')
+        ->select('project_user.*', 'projects.*')
+        ->where('project_user.user_id','=',$user_id)
         ->get();
+
+//     User::join('project_user', 'users.id', '=', 'project_user.user_id')
+//        ->join('projects', 'project_user.project_id', '=', 'projects.id')
+//        ->where('users.id', $user->id)
+//        ->select('projects.name', 'projects.id', 'projects.description')
+//        ->get();
 
     return $projects;
 }
